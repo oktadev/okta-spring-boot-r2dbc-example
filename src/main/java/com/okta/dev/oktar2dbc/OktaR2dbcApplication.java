d6f7a25d47884c7a665fde332751246d932a7959
Matt Raible <matt.raible@okta.com>
2021-10-25 21:39:19 -0600

    Upgrade to Spring Boot 2.5

--- src/main/java/com/okta/dev/oktar2dbc/OktaR2dbcApplication.java
+++ src/main/java/com/okta/dev/oktar2dbc/OktaR2dbcApplication.java
@@ -1,13 +1,8 @@
 package com.okta.dev.oktar2dbc;
 
-import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
@@ -17,16 +12,8 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @SpringBootApplication
 public class OktaR2dbcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OktaR2dbcApplication.class, args);
-	}
-
-	@Bean
-	public ConnectionFactoryInitializer connectionFactoryInitializer(ConnectionFactory connectionFactory) {
-		ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-		initializer.setConnectionFactory(connectionFactory);
-		initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
-		return initializer;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OktaR2dbcApplication.class, args);
+    }
 
 }
